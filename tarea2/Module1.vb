b67904211fa18d6390b5fe55f7022fa6bd8cba1e
szapeta <2375894860101@ingenieria.usac.edu.gt>
2025-02-04 18:02:32 -0600

    Se agrega la tarea 2 para identificar si se da dpi

---
+++ tarea2/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        'Pedir ingresar la edad
+        Console.WriteLine("Ingresa tu edad:")
+
+        'Definir el espacio para edad
+        Dim edad As Integer
+
+        'leer texto ingresado guardarlo en edad
+        edad = Console.ReadLine()
+
+        'comparar si es mayor o igual a 18
+        If edad >= 18 Then
+            'bloque de verdadero
+            Console.WriteLine("Se entrega DPI")
+        Else
+            'bloque falso
+            Console.WriteLine("Se entrega Fe edad")
+        End If
+
+        'acciones finales
+
+        Console.ReadKey()
+    End Sub
+
+End Module
